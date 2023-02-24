3dcdb257f67144ba27f003fbc12cf6f8fe7a53ee
Jannis Weber <jannis.weber@gmx.de>
2023-02-24 09:09:40 +0100

    rails_watch_list

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'lists/new'
+  get 'lists/show'
+  get 'lists/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :movies
+  resources :lists
+  resources :bookmarks
 end
